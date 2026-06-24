66d8193b6252cc9265708cc71bfc7f367e92639b
Ange.N'DAH <angemehola@gmail.com>
2026-06-24 22:02:34 +0200

    ajout des routes et du controller

--- app/Http/Controllers/YoutubeurController.php
+++ app/Http/Controllers/YoutubeurController.php
@@ -3,12 +3,48 @@
 namespace App\Http\Controllers;
 
 use App\Models\Youtubeur;
+use Illuminate\Http\Request;
 
 class YoutubeurController extends Controller
 {
     public function index()
     {
         $youtubeurs = Youtubeur::all();
-        return view('welcome', compact('youtubeurs'));
+        return view('youtubeurs.index', compact('youtubeurs'));
+    }
+
+    public function show($id)
+    {
+        $youtubeur = Youtubeur::findOrFail($id);
+        return view('youtubeurs.show', compact('youtubeur'));
+    }
+
+    public function create()
+    {
+        return view('youtubeurs.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nom'         => 'required|min:2',
+            'role'        => 'required|min:2',
+            'description' => 'required|min:5',
+        ]);
+
+        Youtubeur::create([
+            'nom'         => $request->nom,
+            'role'        => $request->role,
+            'description' => $request->description,
+        ]);
+
+        return redirect('/youtubeurs');
+    }
+
+    public function destroy($id)
+    {
+        $youtubeur = Youtubeur::findOrFail($id);
+        $youtubeur->delete();
+        return redirect('/youtubeurs');
     }
 }
