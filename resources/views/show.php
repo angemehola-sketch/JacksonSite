5a4985efd2978b0f10be60ad7b20ffaed2bde861
Ange.N'DAH <angemehola@gmail.com>
2026-06-26 18:51:51 +0200

    suppression de fichier

--- resources/views/show.php
+++
@@ -1,6 +0,0 @@
-<?php
-$youtubeur = \App\Models\Youtubeur::find($_GET['id']);
-?>
-
-<h2>Description: <?= htmlspecialchars($youtubeur->nom) ?></h2>
-<p><?= htmlspecialchars($youtubeur->description) ?></p>
